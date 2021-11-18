7b1a7c03a17258ad09f1369c6d7f97f960b60e51
Kayne <kayne6@mail.com>
2021-11-18 08:09:05 -0800

    Trying to get this onto my desktop

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -1,20 +1,6 @@
-module.exports.viewAll = function(req, res, next) {
-    const restaurants =[ {
-        id: 1,
-        name: 'Pizza Hut',
-        image: 'https://upload.wikimedia.org/wikipedia/en/2/2a/Mikhail_Gorbachev_in_the_Pizza_Hut_commercial%2C_November_1997.png',
-        rating: 2,
-        category: 'Italian',
-        description: 'Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut Pizza Hut'
-    },
-    {
-        id: 2,
-        name: 'Pizza Hut 2',
-        image: 'https://upload.wikimedia.org/wikipedia/en/2/2a/Mikhail_Gorbachev_in_the_Pizza_Hut_commercial%2C_November_1997.png',
-        rating: 10,
-        category: 'Italian',
-        description: ' Pizza Hut 2'
-    }];
+const {Restaurant} = require('../models')
+module.exports.viewAll = async function(req, res, next) {
+    const restaurants = await Restaurant.findAll();
     res.render('index', {restaurants});
 }
 
