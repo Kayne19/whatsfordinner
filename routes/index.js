69723df5e80d83ea03c7c85e32b66e780ea832cc
Kayne <kayne6@mail.com>
2021-12-03 03:24:47 -0800

    Finished Project

--- routes/index.js
+++ routes/index.js
@@ -4,3 +4,8 @@ const restaurantController = require('../controllers/restaurantController');
 
 router.get('/', restaurantController.viewAll);
 module.exports = router;
+router.get('/edit/:id', restaurantController.renderEditForm);
+router.post('/edit/:id', restaurantController.updateRestaurant);
+router.get('/delete/:id', restaurantController.deleteRestaurant);
+router.get('/add', restaurantController.renderAddForm);
+router.post('/add', restaurantController.addRestaurant);
